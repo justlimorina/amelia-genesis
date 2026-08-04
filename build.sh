af32645a2506e1a2e3cd07cd4447fab94d011d04
Michael Vu <justlimorina@gmail.com>
2026-08-04 17:14:59 +0700

    fix build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+set -e
 
 sudo lb clean
-sudo lb config
+
+sudo lb config \
+    --distribution testing \
+    --architectures amd64 \
+    --archive-areas "main contrib non-free non-free-firmware" \
+    --binary-images iso-hybrid \
+    --bootloaders "grub-efi,syslinux" \
+    --bootappend-live "boot=live components quiet splash username=user user-fullname=Amelia locales=en_US.UTF-8 timezone=Asia/Ho_Chi_Minh"
+
 sudo lb build 2>&1 | tee build.log
